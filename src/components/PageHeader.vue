a1eac07b379088e20a182e392bd59b3edec3931d
Mustafa Baban <mstfababan@gmail.com>
2025-09-24 13:05:45 +0200

    create page header component

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1 class="text-3xl font-semibold">
+        <slot name="title" />
+    </h1>
+</template>
+<script setup lang="ts">
+
+defineSlots<{
+  title: () => string | HTMLElement 
+}>()
+</script>
