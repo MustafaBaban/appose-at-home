ff8ae339c3612b540881ba80580def87e00c95e8
Mustafa Baban <mstfababan@gmail.com>
2025-09-24 13:06:58 +0200

    create user card  component

---
+++ src/components/UserCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flex flex-row items-center gap-4">
+        <img :src="imageSrc" alt="" width="48" height="48" class="rounded-full border border-zinc-200">
+        <div class="info">
+            <p class="text-lg font-semibold">
+                <slot name="name" />
+            </p>
+            <p class="text-sm text-info font-normal">
+                <slot name="email" />
+            </p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+defineSlots<{
+    name: () => string | HTMLElement
+    email: () => string | HTMLElement
+}>()
+
+
+defineProps<{
+    imageSrc: string
+}>()
+
+</script>
+
+<style scoped>
+/* Add your styles here if needed */
+</style>
