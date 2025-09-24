e3732cbd17b4d27c6d1ca28d276021dbb631945d
Mustafa Baban <mstfababan@gmail.com>
2025-09-24 13:06:34 +0200

    create tab header component

---
+++ src/components/TabHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <h5 class="text-lg font-semibold mb-1">
+        <slot name="title" />
+    </h5>
+    <p class="text-sm text-info font-normal">
+        <slot name="subtitle" />
+    </p>
+</template>
+<script setup lang="ts">
+
+defineSlots<{
+    title: () => string | HTMLElement
+    subtitle: () => string | HTMLElement
+}>()
+</script>
