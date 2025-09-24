12a4cadb431386bbcc602982e363ad227327d2a2
Mustafa Baban <mstfababan@gmail.com>
2025-09-24 13:03:21 +0200

    added component structure for the page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
-
+import Settings from './pages/Settings.vue';
 </script>
 
 <template>
+  <div class="font-inter text-neutral-950 container mx-auto  max-sm:px-4 mb-12">
+    <Settings />
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/pages/Settings.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import Header from '../components/PageHeader.vue';
+import AccountManagment from '../tabs/AccountManagment.vue';
+import Accounts from '../tabs/Accounts.vue';
+</script>
+<template>
+    <Header class="mt-12 mb-6">Settings</Header>
+    <AccountManagment />
+    <hr class="my-8">
+    <Accounts />
+
+</template>
+<style scoped></style>
